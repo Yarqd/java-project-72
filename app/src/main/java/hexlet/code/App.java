11fc1a6258cdb461621af937bf48344194a6114b
Ярослав Родионов <iaroslav@MacBook-Pro--Iaroslav.local>
2024-08-29 19:39:56 +0300

    OOO

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -10,8 +10,6 @@ import org.slf4j.LoggerFactory;
 
 import javax.sql.DataSource;
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
