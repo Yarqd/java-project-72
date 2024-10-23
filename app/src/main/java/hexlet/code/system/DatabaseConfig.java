9262175e6f61b19748caccf1d1e6ee68073f41b2
Ярослав Родионов <iaroslav@MacBook-Pro--Iaroslav.local>
2024-10-23 22:05:33 +0300

    fix DB

--- app/src/main/java/hexlet/code/system/DatabaseConfig.java
+++ app/src/main/java/hexlet/code/system/DatabaseConfig.java
@@ -10,20 +10,19 @@ public class DatabaseConfig {
     private static final String H2_USERNAME = "sa";
     private static final String H2_PASSWORD = "";
 
+    private static final String POSTGRES_URL = "jdbc:postgresql://dpg-csck1dij1k6c739buutg-a.frankfurt-postgres.render.com:5432/dbname_w2y3";
+    private static final String POSTGRES_USERNAME = "dbname_w2y3_user";
+    private static final String POSTGRES_PASSWORD = "YWXdq9IHOpaAK7SjA0TF7JgmZNnabXbD";
+
     public static DataSource getDataSource() {
         HikariConfig config = new HikariConfig();
-        String dbUrl = System.getenv("JDBC_DATABASE_URL");
-        if (dbUrl == null || dbUrl.isEmpty()) {
-            dbUrl = H2_URL;
-            config.setUsername(H2_USERNAME);
-            config.setPassword(H2_PASSWORD);
-        } else {
-            System.out.println("Using JDBC_DATABASE_URL: " + dbUrl);
-            config.setUsername(System.getenv("DB_USER"));
-            config.setPassword(System.getenv("DB_PASSWORD"));
-        }
 
+        // Прямое использование PostgreSQL
+        String dbUrl = POSTGRES_URL;
         config.setJdbcUrl(dbUrl);
+        config.setUsername(POSTGRES_USERNAME);
+        config.setPassword(POSTGRES_PASSWORD);
+
         return new HikariDataSource(config);
     }
 }
